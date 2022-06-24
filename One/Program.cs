4ffb28c8abe7a1b93f28de8301b650f1f2484aad
Slavik <slavik-skripka@yandex.ru>
2022-06-24 20:25:56 +0300

    ver 1.0

---
+++ One/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int squareNamber = number * number;
+Console.WriteLine("Квадрат числа равен: " + squareNamber);
